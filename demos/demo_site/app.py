7ba79787302d606486e05e4ad396c880cedc32ad
micro <heromon.9010@gmail.com>
2023-02-22 01:59:53 -0300

    cirando formulario demonstração

---
+++ demos/demo_site/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from mypy_8tml import MyPy8TML
+
+app = Flask(__name__)
+
+form = MyPy8TML()
+
+form.div.in_type('flex-box')\
+        .form.in_class('form')\
+            .h1[' Just a simple form']()\
+            .div.p['e-mail :']().input.in_type('email')(2)\
+            .div.p['password :']().input.in_type('password')(2)\
+            .button.in_type('submit')['submit']()\
+
+
+
+@app.route('/')
+def cass_form():
+    return form.generete()
+
+
+app.run(debug=True)
