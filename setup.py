ca57b1babebef8e249c4daaeb528a67514e8c99d
micro <heromon.9010@gmail.com>
2023-02-26 18:00:16 -0300

    gerando setup do pacote

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+from setuptools import setup
+
+with open('README.md', 'rt') as arq:
+      readme = arq.read()
+
+
+keywords = ['html', 'pyhtml', 'myhtml', 'mypyhmtl', '8tml', 'mypyhtml', 'mypy8tml', '']
+classifiers = [
+          'Development Status :: 1 - Beta',
+          'Intended Audience :: Developers',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Email'
+]
+
+setup(name='mypy8tml',
+      url='https://github.com/MikalROn/MyPy8TML',
+      version='0.1.1',
+      license='MIT license',
+      author='Daniel Coêlho',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author_email='heromon.9010@gmail.com',
+      keywords=keywords,
+      description='A new way to generates html code using class methods in python',
+      packages=['mypy_8tml'],
+      classifiers=classifiers,
+      python_requires='>=3',
+      maintainer='Daniel Coêlho',
+      maintainer_email='heromon.9010@gmail.com',
+      project_urls={
+            'Source': 'https://github.com/MikalROn/MyPy8TML',
+            'Demos': 'https://github.com/MikalROn/MyPy8TML/tree/main/demos/demo_site'
+      }
+)
